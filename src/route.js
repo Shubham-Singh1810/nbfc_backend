14926a042bf8ce4402c78ec2aeb08b95c6feccaa
Trupti Paunikar <tpaunikar92@gmail.com>
2025-03-25 12:59:44 +0530

    after created brand, attribut, attribute setapi

--- src/route.js
+++ src/route.js
@@ -8,15 +8,17 @@ const categoryController = require("./controller/categoryController");
 const subCategory = require("./controller/subCategoryController");
 const brandController = require("./controller/brandController");
 const attributeSetController = require("./controller/attributeSetController");
+const attributeController = require("./controller/attributeController");
 
 router.use("/user", userController);
 router.use("/driver", driverController);
 router.use("/banner", bannerController);
 router.use("/vender", venderController);
 router.use("/category", categoryController);
-router.use("/subCategory", subCategory);
+router.use("/sub-category", subCategory);
 router.use("/brand", brandController);
 router.use("/attribute-set", attributeSetController);
+router.use("/attribute", attributeController);
 
 
 module.exports = router;
