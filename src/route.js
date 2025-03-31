de117485c4a81e8f077e641a2880944733596c72
Trupti Paunikar <tpaunikar92@gmail.com>
2025-03-31 15:16:25 +0530

    after creating product type and location api

--- src/route.js
+++ src/route.js
@@ -13,6 +13,8 @@ const adminController = require("./controller/adminController");
 const productController = require("./controller/productController");
 const addressController = require("./controller/addressController");
 const tagController = require("./controller/tagController");
+const locationController = require("./controller/locationController");
+const productTypeController = require("./controller/productTypeController");
 
 router.use("/user", userController);
 router.use("/driver", driverController);
@@ -27,6 +29,8 @@ router.use("/attribute", attributeController);
 router.use("/product", productController);
 router.use("/address", addressController);
 router.use("/tag", tagController);
+router.use("/location", locationController);
+router.use("/product-type", productTypeController);
 
 
 module.exports = router;
