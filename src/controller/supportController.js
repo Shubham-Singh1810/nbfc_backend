1211a7f4bbda2f847f12c21c030644279f500feb
Shubham <hittheshubham1810@gmail.com>
2025-04-14 10:48:00 +0530

    after adding the list api for faq

--- src/controller/supportController.js
+++ src/controller/supportController.js
@@ -11,7 +11,8 @@ supportController.post("/add-contact-query", async (req, res) => {
   try {
     const contactCreated = await Contact.create(req.body);
     sendResponse(res, 200, "Success", {
-      message: "We have recived your query, Someone from our team will reach back to you!",
+      message:
+        "We have recived your query, Someone from our team will reach back to you!",
       data: contactCreated,
       statusCode: 200,
     });
@@ -38,64 +39,6 @@ supportController.get("/list-contact-query", async (req, res) => {
       statusCode: 500,
     });
   }
-});
-supportController.post("/create-faq", async (req, res) => {
-  try {
-    const faqCreated = await Faq.create(req.body);
-    sendResponse(res, 200, "Success", {
-      message: "Faq added successfully",
-      data: faqCreated,
-      statusCode: 200,
-    });
-  } catch (error) {
-    console.error(error);
-    sendResponse(res, 500, "Failed", {
-      message: error.message || "Internal server error",
-      statusCode: 500,
-    });
-  }
-});
-supportController.post("/list-faq", async (req, res) => {
-  let query = {};
-  if (req.body.category){
-    query.category = req?.body?.category
-  }
-  try {
-    const faqList = await Faq.find(query);
-    sendResponse(res, 200, "Success", {
-      message: "Faq list retrived successfully.",
-      data: faqList,
-      statusCode: 200,
-    });
-  } catch (error) {
-    console.error(error);
-    sendResponse(res, 500, "Failed", {
-      message: error.message || "Internal server error",
-      statusCode: 500,
-    });
-  }
-});
-supportController.delete("/delete-faq/:id", async (req, res) => {
- try {
-     const { id } = req.params;
-     const faq = await Faq.findById(id);
-     if (!faq) {
-       return sendResponse(res, 404, "Failed", {
-         message: "Faq not found",
-       });
-     }
-     await Faq.findByIdAndDelete(id);
-     sendResponse(res, 200, "Success", {
-       message: "Faq deleted successfully!",
-       statusCode: 200,
-     });
-   } catch (error) {
-     console.error(error);
-     sendResponse(res, 500, "Failed", {
-       message: error.message || "Internal server error",
-       statusCode: 200,
-     });
-   }
 });
 supportController.get("/details", async (req, res) => {
   try {
@@ -166,6 +109,110 @@ supportController.get("/privacy-policy", async (req, res) => {
     sendResponse(res, 500, "Failed", {
       message: error.message || "Internal server error",
       statusCode: 500,
+    });
+  }
+});
+
+supportController.post("/create-faq", async (req, res) => {
+  try {
+    const faqCreated = await Faq.create(req.body);
+    sendResponse(res, 200, "Success", {
+      message: "Faq added successfully",
+      data: faqCreated,
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode: 500,
+    });
+  }
+});
+supportController.put("/update-faq", async (req, res) => {
+  try {
+    const id = req.body._id;
+    const faq = await Faq.findById(id);
+    if (!faq) {
+      return sendResponse(res, 404, "Failed", {
+        message: "Faq not found",
+        statusCode: 403,
+      });
+    }
+    const updatedFaq = await Faq.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    sendResponse(res, 200, "Success", {
+      message: "Faq updated successfully!",
+      data: updatedFaq,
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode: 500,
+    });
+  }
+});
+supportController.post("/list-faq", async (req, res) => {
+  try {
+    const {
+      searchKey = "",
+      category,
+      pageNo = 1,
+      pageCount = 10,
+      sortByField,
+      sortByOrder,
+    } = req.body;
+    const query = {};
+    if (category) query.category = category;
+    if (searchKey) query.question = { $regex: searchKey, $options: "i" };
+    const sortField = sortByField || "createdAt";
+    const sortOrder = sortByOrder === "asc" ? 1 : -1;
+    const sortOption = { [sortField]: sortOrder };
+    const faqList = await Faq.find(query)
+      .sort(sortOption)
+      .limit(parseInt(pageCount))
+      .skip(parseInt(pageNo - 1) * parseInt(pageCount));
+    const userFaq = await Faq.countDocuments({ category: "user" });
+    const driverFaq = await Faq.countDocuments({ category: "driver" });
+    const vendorFaq = await Faq.countDocuments({ category: "vendor" });
+
+    sendResponse(res, 200, "Success", {
+      message: "Faq list retrived successfully.",
+      data: faqList,
+      documentCount:{userFaq, driverFaq, vendorFaq},
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode: 500,
+    });
+  }
+});
+supportController.delete("/delete-faq/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const faq = await Faq.findById(id);
+    if (!faq) {
+      return sendResponse(res, 404, "Failed", {
+        message: "Faq not found",
+      });
+    }
+    await Faq.findByIdAndDelete(id);
+    sendResponse(res, 200, "Success", {
+      message: "Faq deleted successfully!",
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode: 200,
     });
   }
 });
