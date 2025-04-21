00951814c1e81b62767581cf3a9b92d089b0c52b
Shubham <hittheshubham1810@gmail.com>
2025-04-21 14:48:40 +0530

    after adding the api of admin list

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -58,4 +58,53 @@ adminController.post("/login", async (req, res) => {
       });
     }
 });
+
+adminController.post("/list", async (req, res) => {
+  try {
+    const {
+      searchKey = "",
+      status,
+      pageNo = 1,
+      pageCount = 10,
+      sortByField,
+      sortByOrder,
+    } = req.body;
+
+    const query = {};
+    if (status) query.profileStatus = status;
+    if (searchKey) {
+      query.$or = [
+        { firstName: { $regex: searchKey, $options: "i" } },
+        { lastName: { $regex: searchKey, $options: "i" } },
+        { email: { $regex: searchKey, $options: "i" } },
+      ];
+    }
+
+    // Construct sorting object
+    const sortField = sortByField || "createdAt";
+    const sortOrder = sortByOrder === "asc" ? 1 : -1;
+    const sortOption = { [sortField]: sortOrder };
+
+    // Fetch the category list
+    const adminList = await Admin.find(query)
+      .sort(sortOption)
+      .limit(parseInt(pageCount))
+      .skip(parseInt(pageNo - 1) * parseInt(pageCount))
+      
+    const totalCount = await Admin.countDocuments({});
+    sendResponse(res, 200, "Success", {
+      message: "Admin list retrieved successfully!",
+      data: adminList,
+      documentCount: {
+        totalCount,
+      },
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+    });
+  }
+});
 module.exports = adminController;
