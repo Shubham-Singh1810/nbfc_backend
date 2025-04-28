51e2a61807de59502a9cec60aeb7ddcb7c58e1de
Shubham <hittheshubham1810@gmail.com>
2025-04-28 17:45:08 +0530

    after adding admin update api

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -13,13 +13,31 @@ adminController.post("/create", async (req, res) => {
     sendResponse(res, 200, "Success", {
       message: "Admin created successfully!",
       data: AdminData,
-      statusCode:200
+      statusCode: 200,
     });
   } catch (error) {
     console.error(error);
     sendResponse(res, 500, "Failed", {
       message: error.message || "Internal server error",
-      statusCode:500
+      statusCode: 500,
+    });
+  }
+});
+adminController.put("/update", async (req, res) => {
+  try {
+    const AdminData = await Admin.findByIdAndUpdate(req?.body?._id, req.body, {
+      new: true,
+    });
+    sendResponse(res, 200, "Success", {
+      message: "Admin updated successfully!",
+      data: AdminData,
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode: 500,
     });
   }
 });
@@ -32,11 +50,11 @@ adminController.delete("/delete/:id", async (req, res) => {
         message: "Admin not found",
       });
     }
-    
+
     await Admin.findByIdAndDelete(id);
     sendResponse(res, 200, "Success", {
       message: "Admin deleted successfully!",
-      statusCode:200
+      statusCode: 200,
     });
   } catch (error) {
     console.error(error);
@@ -47,29 +65,29 @@ adminController.delete("/delete/:id", async (req, res) => {
 });
 
 adminController.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await Admin.findOne({ email, password }).populate({
-        path: "role",
-      });;
-      if (user) {
-        return sendResponse(res, 200, "Success", {
-          message: "User logged in successfully",
-          data: user,
-          statusCode: 200,
-        });
-      } else {
-        return sendResponse(res, 422, "Failed", {
-          message: "Invalid Credentials",
-          statusCode: 422,
-        });
-      }
-    } catch (error) {
-      return sendResponse(res, 500, "Failed", {
-        message: error.message || "Internal server error.",
-        statusCode: 500,
+  try {
+    const { email, password } = req.body;
+    const user = await Admin.findOne({ email, password }).populate({
+      path: "role",
+    });
+    if (user) {
+      return sendResponse(res, 200, "Success", {
+        message: "User logged in successfully",
+        data: user,
+        statusCode: 200,
+      });
+    } else {
+      return sendResponse(res, 422, "Failed", {
+        message: "Invalid Credentials",
+        statusCode: 422,
       });
     }
+  } catch (error) {
+    return sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error.",
+      statusCode: 500,
+    });
+  }
 });
 
 adminController.post("/list", async (req, res) => {
