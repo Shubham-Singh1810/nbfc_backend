2ce53856d27b043a42e8b0eaf1d85329322403e5
Shubham <hittheshubham1810@gmail.com>
2025-04-29 11:08:42 +0530

    after adding the

--- src/model/booking.Schema.js
+++ src/model/booking.Schema.js
@@ -2,49 +2,62 @@ const mongoose = require("mongoose");
 const timestamps = require("mongoose-timestamp");
 
 const bookingSchema = mongoose.Schema({
-    totalAmount: {
+  totalAmount: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["orderPlaced", "partiallyDelivered", "completed", "cancelled"],
+  },
+  signature: {
+    type: String,
+    require: true,
+  },
+  orderId: {
+    type: String,
+    require: true,
+  },
+  modeOfPayment: {
+    type: String,
+    enum: ["COD", "Online"],
+  },
+  paymentId: {
+    type: String,
+  },
+  product: [
+    {
+      productId: { type: String, ref: "Product" },
+      quantity: { type: Number },
+      totalPrice: { type: Number },
+      deliveryStatus: {
         type: String,
-    },
-    status: {
-        type: String,
-        require: true,
-    },
-    signature: {
-        type: String,
-        require: true,
-    },
-    orderId: {
-        type: String,
-        require: true,
-    },
-    modeOfPayment:{
-        type: String,
-        enum: ["COD", "Online"],
-    },
-    paymentId:{
-        type: String
-    },
-    product:  [
-        {productId:{ type: String, ref: "Product" },
-        quantity:{ type: Number },
-        totalPrice:{ type: Number },
-        deliveryStatus:{ type: Boolean, default:false },
-        driverId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Driver",
-            required: true, 
-        },
-    }],
-    userId: {
+        enum: [
+          "orderPlaced",
+          "orderPacked",
+          "driverAssigned",
+          "driverAccepted",
+          "pickedOrder",
+          "completed",
+          "cancelled",
+        ],
+      },
+      driverId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true, 
+        ref: "Driver",
+        required: true,
+      },
     },
-    addressId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Address",
-        required: true, 
-    },
+  ],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  addressId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+    required: true,
+  },
 });
 
 bookingSchema.plugin(timestamps);
