ce510aae00612ce7efc0587720236c9006e2a184
Trupti Paunikar <tpaunikar92@gmail.com>
2025-05-06 16:31:14 +0530

    created order api

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,12 +1,12 @@
 const Notification = require("../model/notification.Schema");
 const admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+// var serviceAccount = require("./serviceAccountKey.json");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
 
 exports.sendNotification = async (data) => {
   try {
@@ -14,16 +14,16 @@ exports.sendNotification = async (data) => {
     // io.emit("notificationCreated", {
     //   message: "A New Notification Added",
     // });
-    const message = {
-      notification: {
-        title: data?.title || "Default Title",
-        body: data?.subTitle || "Default Body",
-        image: data?.icon || "Default Body",
-      },
-      token:
-        "fCBfyfuaAl0FeG6e93S5mc:APA91bEWMG6tNIshaebx07iOP3lD537F-QOdgn_Wcl7unSBhjeuUzLNnUZccLDdbjb9ff-hg47alk9rJT-9bNYK_AwGaaGknvXgAgyMfkuo090qOjfEfTys",
-    };
-    const response = await admin.messaging().send(message);
+    // const message = {
+    //   notification: {
+    //     title: data?.title || "Default Title",
+    //     body: data?.subTitle || "Default Body",
+    //     image: data?.icon || "Default Body",
+    //   },
+    //   token:
+    //     "fCBfyfuaAl0FeG6e93S5mc:APA91bEWMG6tNIshaebx07iOP3lD537F-QOdgn_Wcl7unSBhjeuUzLNnUZccLDdbjb9ff-hg47alk9rJT-9bNYK_AwGaaGknvXgAgyMfkuo090qOjfEfTys",
+    // };
+    // const response = await admin.messaging().send(message);
     return notificationCreated;
   } catch (error) {
     console.error("Error creating notification:", error);
