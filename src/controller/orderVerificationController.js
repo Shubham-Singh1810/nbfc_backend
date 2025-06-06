01077fd101f6f8a21a625ae2eecc02b37348abdb
Shubham <hittheshubham1810@gmail.com>
2025-06-06 20:01:29 +0530

    after updaing api

--- src/controller/orderVerificationController.js
+++ src/controller/orderVerificationController.js
@@ -16,82 +16,93 @@ const { generateOTP } = require("../utils/common");
 const axios = require("axios");
 const { json } = require("stream/consumers");
 
+orderVerificationController.post(
+  "/create",
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      const { productIdArr, orderId, driverId } = req.body;
+      let productIds = JSON.parse(productIdArr);
 
-orderVerificationController.post("/create", upload.single("image"), async (req, res) => {
-  try {
-    const { productIdArr, orderId, driverId } = req.body;
-    let productIds = JSON.parse(productIdArr);
+      if (!orderId || !productIds || productIds.length === 0 || !driverId) {
+        return sendResponse(res, 400, "Failed", {
+          message: "Missing order ID, product IDs array, or driver ID.",
+        });
+      }
 
-    if (!orderId || !productIds || productIds.length === 0 || !driverId) {
-      return sendResponse(res, 400, "Failed", {
-        message: "Missing order ID, product IDs array, or driver ID.",
+      // Validate order
+      const orderDetails = await Booking.findOne({ _id: orderId }).populate({
+        path: "userId",
+        select: "name description phone",
+      });
+
+      if (!orderDetails) {
+        return sendResponse(res, 404, "Failed", {
+          message: "Order ID not found",
+          statusCode: 404,
+        });
+      }
+
+      // Upload image
+      let imageUrl = null;
+      if (req.file) {
+        const image = await cloudinary.uploader.upload(req.file.path);
+        imageUrl = image.url;
+      }
+
+      // Generate OTP
+      const phoneOtp = generateOTP();
+      const appHash = "ems/3nG2V1H";
+      const otpMessage = `<#> ${phoneOtp} is your OTP for verification. Do not share it with anyone.\n${appHash}`;
+
+      // Create entries for each productId
+      const createdEntries = await Promise.all(
+        productIds.map((productId) =>
+          OrderVerification.create({
+            productId,
+            orderId,
+            driverId,
+            image: imageUrl,
+            otp: phoneOtp,
+          })
+        )
+      );
+
+      // Send OTP
+      const otpResponse = await axios.post(
+        `https://api.authkey.io/request?authkey=${
+          process.env.AUTHKEY_API_KEY
+        }&mobile=${orderDetails?.userId?.phone}&country_code=91&sid=${
+          process.env.AUTHKEY_SENDER_ID
+        }&company=Acediva&otp=${phoneOtp}&message=${encodeURIComponent(
+          otpMessage
+        )}`
+      );
+
+      if (
+        otpResponse?.status === 200 ||
+        otpResponse?.data?.type === "success"
+      ) {
+        return sendResponse(res, 200, "Success", {
+          message: "OTP sent successfully",
+          data: createdEntries,
+          statusCode: 200,
+        });
+      } else {
+        return sendResponse(res, 422, "Failed", {
+          message: "Unable to send OTP",
+          statusCode: 422,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      sendResponse(res, 500, "Failed", {
+        message: error.message || "Internal server error",
+        statusCode: 500,
       });
     }
-
-    // Validate order
-    const orderDetails = await Booking.findOne({ _id: orderId }).populate({
-      path: "userId",
-      select: "name description phone",
-    });
-
-    if (!orderDetails) {
-      return sendResponse(res, 404, "Failed", {
-        message: "Order ID not found",
-        statusCode: 404,
-      });
-    }
-
-    // Upload image
-    let imageUrl = null;
-    if (req.file) {
-      const image = await cloudinary.uploader.upload(req.file.path);
-      imageUrl = image.url;
-    }
-
-    // Generate OTP
-    const phoneOtp = generateOTP();
-    const appHash = "ems/3nG2V1H";
-    const otpMessage = `<#> ${phoneOtp} is your OTP for verification. Do not share it with anyone.\n${appHash}`;
-
-    // Create entries for each productId
-    const createdEntries = await Promise.all(
-      productIds.map((productId) =>
-        OrderVerification.create({
-          productId,
-          orderId,
-          driverId,
-          image: imageUrl,
-          otp: phoneOtp,
-        })
-      )
-    );
-
-    // Send OTP
-    const otpResponse = await axios.post(
-      `https://api.authkey.io/request?authkey=${process.env.AUTHKEY_API_KEY}&mobile=${orderDetails?.userId?.phone}&country_code=91&sid=${process.env.AUTHKEY_SENDER_ID}&company=Acediva&otp=${phoneOtp}&message=${encodeURIComponent(otpMessage)}`
-    );
-
-    if (otpResponse?.status === 200 || otpResponse?.data?.type === "success") {
-      return sendResponse(res, 200, "Success", {
-        message: "OTP sent successfully",
-        data: createdEntries,
-        statusCode: 200,
-      });
-    } else {
-      return sendResponse(res, 422, "Failed", {
-        message: "Unable to send OTP",
-        statusCode: 422,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    sendResponse(res, 500, "Failed", {
-      message: error.message || "Internal server error",
-      statusCode: 500,
-    });
   }
-});
-
+);
 
 // orderVerificationController.put("/verify-otp", async (req, res) => {
 //   try {
@@ -160,11 +171,6 @@ orderVerificationController.post("/create", upload.single("image"), async (req, 
 //     });
 //   }
 // });
-
-
-
-
-
 
 // orderVerificationController.put("/verify-otp", async (req, res) => {
 //   try {
@@ -311,7 +317,6 @@ orderVerificationController.post("/create", upload.single("image"), async (req, 
 //   }
 // });
 
-
 orderVerificationController.put("/verify-otp", async (req, res) => {
   try {
     const { productIdArr, orderId, otp } = req.body;
@@ -321,10 +326,14 @@ orderVerificationController.put("/verify-otp", async (req, res) => {
         message: "Missing order ID, product IDs array, or OTP.",
       });
     }
-
+    let driverDetails ;
     const updatedEntries = await Promise.all(
       productIdArr.map(async (productId) => {
-        const order = await OrderVerification.findOne({ otp, productId, orderId });
+        const order = await OrderVerification.findOne({
+          otp,
+          productId,
+          orderId,
+        });
         if (!order) return null;
 
         await Booking.findOneAndUpdate(
@@ -333,9 +342,13 @@ orderVerificationController.put("/verify-otp", async (req, res) => {
           { new: true }
         );
 
-        await OrderVerification.findByIdAndUpdate(order._id, { isOtpVerified: true });
+        await OrderVerification.findByIdAndUpdate(order._id, {
+          isOtpVerified: true,
+        });
 
-        const booking = await Booking.findById(orderId).populate("product.productId");
+        const booking = await Booking.findById(orderId).populate(
+          "product.productId"
+        );
         const matchedProduct = booking.product.find(
           (p) => String(p.productId._id) === String(productId)
         );
@@ -346,26 +359,31 @@ orderVerificationController.put("/verify-otp", async (req, res) => {
 
         const vendorId = productInfo.createdBy;
         const driverId = matchedProduct.driverId;
+        driverDetails = await Driver.findOne({_id:driverId})
 
         const adminFund = await AdminFund.findOne();
-        const venderDetails = await Vender.findOne({_id:vendorId});
+        const venderDetails = await Vender.findOne({ _id: vendorId });
         if (!adminFund) return null;
 
-        const vendorPercentage = parseFloat(venderDetails.venderCommision || adminFund.venderCommision);
-        const driverCommision = parseFloat(adminFund.driverCommision || 5);
+        const vendorPercentage = parseFloat(
+          venderDetails.venderCommision || adminFund.venderCommision
+        );
+        
         const adminPercentage = 100 - vendorPercentage;
 
         const vendorAmount = (price * vendorPercentage) / 100;
         const adminAmount = (price * adminPercentage) / 100;
-        const driverAmount = driverCommision * 10
-        const adminWalletAmount = adminFund.wallet - (vendorAmount + driverAmount);
+
+        const adminWalletAmount = adminFund.wallet - vendorAmount;
 
         // Admin Wallet Update
-        adminFund.wallet = adminWalletAmount
-        adminFund.totalEarnings = adminFund.totalEarnings - (vendorAmount + driverAmount);
+        adminFund.wallet = adminWalletAmount;
+        adminFund.totalEarnings = adminFund.totalEarnings - vendorAmount;
         adminFund.transactionHistory = adminFund.transactionHistory || [];
         adminFund.transactionHistory.push({
-          message: `Admin shared ₹${vendorAmount.toFixed(2)} to vendor and  ₹${driverAmount.toFixed(2)} to driver for order ${orderId}`,
+          message: `Admin shared ₹${vendorAmount.toFixed(
+            2
+          )} to vendor for order ID ${orderId}`,
           transactionType: "debit",
           date: moment().format("YYYY-MM-DD HH:mm:ss"),
         });
@@ -373,46 +391,59 @@ orderVerificationController.put("/verify-otp", async (req, res) => {
 
         // Vendor Wallet Update
         if (vendorId) {
-          const vendor = await Vender.findById(vendorId);
-          if (vendor) {
-            vendor.wallet = (
-              parseFloat(vendor.wallet || 0) + vendorAmount
+          if (venderDetails) {
+            venderDetails.wallet = (
+              parseFloat(vendorAmount.wallet || 0) + vendorAmount
             ).toFixed(2);
-            vendor.transactionHistory = vendor.transactionHistory || [];
-            vendor.transactionHistory.push({
-              message: `₹${vendorAmount.toFixed(2)} credited for delivered product ${productId}`,
+            venderDetails.transactionHistory =
+              vendorAmount.transactionHistory || [];
+            venderDetails.totalEarnings =
+              venderDetails.totalEarnings + vendorAmount;
+            venderDetails.transactionHistory.push({
+              message: `₹${vendorAmount.toFixed(
+                2
+              )} credited for delivered product ID ${productId}`,
               transactionType: "credit",
               amount: vendorAmount.toFixed(2),
               date: moment().format("YYYY-MM-DD HH:mm:ss"),
             });
-            await vendor.save();
-          }
-        }
-
-        // Driver Wallet Update
-        if (driverId) {
-          const driver = await Driver.findById(driverId);
-          if (driver) {
-            driver.wallet = (
-              parseFloat(driver.wallet || 0) + driverAmount
-            ).toFixed(2);
-            driver.transactionHistory = driver.transactionHistory || [];
-            driver.transactionHistory.push({
-              message: `₹${driverAmount.toFixed(2)} credited for delivering product ${productId}`,
-              transactionType: "credit",
-              amount: driverAmount.toFixed(2),
-              date: moment().format("YYYY-MM-DD HH:mm:ss"),
-            });
-            await driver.save();
+            await venderDetails.save();
           }
         }
 
         return order;
       })
     );
+    const adminFund = await AdminFund.findOne();
+    const driverAmount =  adminFund?.driverCommission * 10
 
+    driverDetails?.wallet = driverDetails?.wallet + driverAmount;
+    driverDetails?.totalEarnings = driverDetails?.totalEarnings + driverAmount;
+     driverDetails.transactionHistory.push({
+              message: `₹${driverAmount.toFixed(
+                2
+              )} has been credited for delivering the order ID ${orderId}`,
+              transactionType: "credit",
+              
+              date: moment().format("YYYY-MM-DD HH:mm:ss"),
+            });
+    await driverDetails.save();
+
+    // Admin Wallet Update
+        adminFund.wallet = adminFund?.wallet-driverAmount;
+        adminFund.totalEarnings = adminFund.totalEarnings - driverAmount;
+        adminFund.transactionHistory = adminFund.transactionHistory || [];
+        adminFund.transactionHistory.push({
+          message: `Admin shared ₹${driverAmount.toFixed(
+            2
+          )} to driver for order ID ${orderId}`,
+          transactionType: "debit",
+          date: moment().format("YYYY-MM-DD HH:mm:ss"),
+        });
+        await adminFund.save();
+   
     const verifiedCount = updatedEntries.filter(Boolean).length;
-
+    
     if (verifiedCount > 0) {
       return sendResponse(res, 200, "Success", {
         message: `OTP verified and payment distributed for ${verifiedCount} product(s).`,
@@ -430,6 +461,5 @@ orderVerificationController.put("/verify-otp", async (req, res) => {
     });
   }
 });
-
 
 module.exports = orderVerificationController;
